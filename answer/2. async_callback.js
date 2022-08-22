46a8dff77de5c5a78f98fb6f6f67ca484feefed4
fornacy <>
2022-08-22 14:49:31 +0800

    修正程式終止判斷邏輯

--- answer/2. async_callback.js
+++ answer/2. async_callback.js
@@ -8,31 +8,6 @@ const db = mongoose.connection
 // 連接資料庫: db.once('open', callback)
 // callback function是代替設定等待時間的function，server完成後會給予一個ack，告知client完成
 db.once('open', () => {
-<<<<<<< HEAD
-  for (const [user_index, user] of users.entries()) {
-    // 創建使用者資料(user)
-    UserModel.create(user, (err, user) => {
-      // 對每個user建立相對應餐廳資料
-      console.log('user created')
-      const userRestaurant = []
-      restaurants.forEach((restaurant, rest_index) => {
-        if (rest_index >= 3 * user_index && rest_index < 3 * (user_index + 1)) {
-          restaurant.userId = user._id
-          userRestaurant.push(restaurant)
-        }
-      })
-      RestaurantModel.create(userRestaurant, () => {
-        console.log('restaurant created')
-        // 等待所有使用者的餐廳資料創建完成
-        if (user_index >= users.length - 1) {
-          console.log('所有使用者與餐廳資料創建完成')
-          process.exit()
-        }
-      })
-    })
-  }
-})
-=======
     for (const [user_index, user] of users.entries()) {
         //創建使用者資料(user)
         UserModel.create(user, (err, user)=>{
@@ -60,4 +35,3 @@ db.once('open', () => {
         })
     }
 })
->>>>>>> e2d26583c27f9b19a96c08cc1b0add8b05baab55
